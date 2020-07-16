88da22e7c3015968604ddb4f42d0d931be5377ec
MiranDaniel <mirandanielcz@gmail.com>
2020-07-16 14:32:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
-sudo ./ssh.sh
-sudo ./protonvpn.sh
+sudo apt install python3-pip
+sudo apt install -y openvpn dialog python3-pip python3-setuptools
+sudo pip3 install protonvpn-cli
+sudo apt update
+sudo apt install openssh-server
+sudo systemctl status ssh
+sudo ufw allow ssh
+sudo protonvpn init
